97238cc7cdbcddb10fe1783dc0299709d2d06122
Mylinh Vu <mylinhvu@berkeley.edu>
2023-05-01 18:59:38 -0700

    attempted fixing ogre

--- RadialBasisFunction.h
+++ RadialBasisFunction.h
@@ -5,7 +5,7 @@
 #ifndef FINALPROJ_SPATIALKEYFRAMING_SP23_CS184_RADIALBASISFUNCTION_H
 #define FINALPROJ_SPATIALKEYFRAMING_SP23_CS184_RADIALBASISFUNCTION_H
 
-#include "Ogre.h"
+#include "ogre-13.6.4/OgreMain/include/Ogre.h"
 #include <vector>
 #include <iostream>
 #include "SpatialKeyframing.h"
