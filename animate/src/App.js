06c6bafc024b22897a061d973e7ae373b30fca8a
Armaan Goel <armaangoel@Armaans-MacBook-Air.local>
2023-05-04 00:04:14 -0700

    added assets

--- animate/src/App.js
+++ animate/src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import ModelViewer from 'react-model-viewer';
+import scene from "./assets/scene.gltf";
 
-const modelPath = 'https://raw.githubusercontent.com/Setsun/static-3d-assets/master/gtlf/sonic/scene.gltf';
+const modelPath = '/Users/armaangoel/Desktop/184/finalproj-spatialkeyframing/animate/src/assets/scene.gltf';
 
 function App() {
   return (
-    <ModelViewer type="gtlf" src={modelPath} />
+    <div style={{padding: "10%"}}>
+      <ModelViewer type="gtlf" src={scene} />
+    </div>
   );
 }
 
