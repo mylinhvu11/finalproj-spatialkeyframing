b41d6f0164613a1755e50c217134b5910993781c
Armaan Goel <armaangoel@Armaans-MacBook-Air.local>
2023-05-04 00:07:16 -0700

    fix scene.gltf

--- animate/src/App.js
+++ animate/src/App.js
@@ -1,13 +1,12 @@
 import './App.css';
 import ModelViewer from 'react-model-viewer';
-import scene from "./assets/scene.gltf";
 
-const modelPath = '/Users/armaangoel/Desktop/184/finalproj-spatialkeyframing/animate/src/assets/scene.gltf';
+const modelPath = 'https://raw.githubusercontent.com/mylinhvu11/finalproj-spatialkeyframing/main/animate/src/assets/scene.gltf';
 
 function App() {
   return (
     <div style={{padding: "10%"}}>
-      <ModelViewer type="gtlf" src={scene} />
+      <ModelViewer type="gtlf" src={modelPath} />
     </div>
   );
 }
